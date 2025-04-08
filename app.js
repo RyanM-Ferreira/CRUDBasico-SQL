5e80ab1fc852b3d1c71c668d7e61fe440d302701
Ryan Ferreira <RyanMatheusFerreira@outlook.com.br>
2025-04-08 00:35:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
 const express = require("express");
 const path = require("path");
-const db = require("./models");
-
 const app = express();
-
+const db = require("./models");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
+// Configuração do EJS como view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// Rota principal
 const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
+
+// Rotas para categorias e produtos
 const categoriaRouter = require("./routes/categorias");
 const alunoRouter = require("./routes/alunos");
 const professorRouter = require("./routes/professores");
 const groupRouter = require("./routes/group");
 
-app.use("/", indexRouter);
+//const produtoRouter = require("./routes/produtos");
 app.use("/categorias", categoriaRouter);
 app.use("/alunos", alunoRouter);
 app.use("/professores", professorRouter);
 app.use("/group", groupRouter);
 
-db.sequelize.sync()
-  .then(() => {
-    console.log("Banco de dados sincronizado");
+//app.use("/produtos", produtoRouter);
 
-    // Só inicia o servidor se estiver rodando localmente
-    if (process.env.LOCAL === "true") {
-      app.listen(3000, () => {
-        console.log("Servidor rodando localmente na porta 3000");
-      });
-    }
-  })
-  .catch(err => {
-    console.error("Erro ao sincronizar com o banco:", err);
-  });
+// Iniciar o servidor e sincronizar com o banco de dados
+/*db.sequelize.sync().then(() => {
+    app.listen(3000, () => {
+        console.log("Servidor em execução na porta 3000");
+    });
+});*/
+
+// Iniciar o servidor e sincronizar com o banco de dados (eu vou ficar malucokkkkkkk)
+db.sequelize.sync()
+    .then(() => {
+        console.log("Banco de dados sincronizado");
+
+        // Só inicia o servidor se rodar com: node app.js --local
+        if (process.argv.includes("--local")) {
+            app.listen(3000, () => {
+                console.log("Servidor rodando em http://localhost:3000 🚀");
+            });
+        }
+    })
+    .catch(err => {
+        console.error("Erro ao sincronizar o banco de dados:", err);
+    });
 
 module.exports = app;
